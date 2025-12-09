aa7c6d30fe5b881ff0220bc51f3850da0c7d4df9
Kobeep <kuba.pospieszny@gmail.com>
2025-12-09 10:32:12 +0100

    Add OCI client structure with authentication support

--- go.mod
+++ go.mod
@@ -3,19 +3,17 @@ module github.com/Kobeep/cloud-dr-orchestrator
 go 1.24.10
 
 require (
-	github.com/fsnotify/fsnotify v1.9.0 // indirect
-	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/oracle/oci-go-sdk/v65 v65.105.0
+	github.com/spf13/cobra v1.10.2
+)
+
+require (
+	github.com/gofrs/flock v0.10.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
-	github.com/sagikazarmark/locafero v0.11.0 // indirect
-	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
-	github.com/spf13/afero v1.15.0 // indirect
-	github.com/spf13/cast v1.10.0 // indirect
-	github.com/spf13/cobra v1.10.2 // indirect
+	github.com/sony/gobreaker v0.5.0 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
-	github.com/spf13/viper v1.21.0 // indirect
-	github.com/subosito/gotenv v1.6.0 // indirect
-	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.28.0 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	golang.org/x/crypto v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
 )
---
+++ pkg/oracle/client.go
@@ -0,0 +1,85 @@
+package oracle
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/oracle/oci-go-sdk/v65/common"
+	"github.com/oracle/oci-go-sdk/v65/objectstorage"
+)
+
+// Client represents an Oracle Cloud Infrastructure client for Object Storage operations
+type Client struct {
+	objectStorageClient *objectstorage.ObjectStorageClient
+	namespace           string
+	bucketName          string
+	compartmentID       string
+}
+
+// Config holds the configuration for OCI client
+type Config struct {
+	ConfigFilePath string
+	Profile        string
+	Namespace      string
+	BucketName     string
+	CompartmentID  string
+}
+
+// NewClient creates a new OCI Object Storage client
+// It reads credentials from ~/.oci/config or uses environment variables
+func NewClient(config Config) (*Client, error) {
+	var configProvider common.ConfigurationProvider
+	var err error
+
+	// Try to load from config file first
+	if config.ConfigFilePath == "" {
+		config.ConfigFilePath = common.DefaultConfigFilePath()
+	}
+	if config.Profile == "" {
+		config.Profile = "DEFAULT"
+	}
+
+	configProvider, err = common.ConfigurationProviderFromFileWithProfile(
+		config.ConfigFilePath,
+		config.Profile,
+		"",
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load OCI config: %w", err)
+	}
+
+	// Create Object Storage client
+	client, err := objectstorage.NewObjectStorageClientWithConfigurationProvider(configProvider)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Object Storage client: %w", err)
+	}
+
+	// Get namespace if not provided
+	namespace := config.Namespace
+	if namespace == "" {
+		ctx := context.Background()
+		request := objectstorage.GetNamespaceRequest{}
+		response, err := client.GetNamespace(ctx, request)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get namespace: %w", err)
+		}
+		namespace = *response.Value
+	}
+
+	return &Client{
+		objectStorageClient: &client,
+		namespace:           namespace,
+		bucketName:          config.BucketName,
+		compartmentID:       config.CompartmentID,
+	}, nil
+}
+
+// GetBucketName returns the configured bucket name
+func (c *Client) GetBucketName() string {
+	return c.bucketName
+}
+
+// GetNamespace returns the OCI namespace
+func (c *Client) GetNamespace() string {
+	return c.namespace
+}
